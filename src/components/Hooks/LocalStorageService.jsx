ad4b321d39eb4d59dc1d9b3e2491f22db5a1ebae
valar__morghulis <111417495+AndriiKachan1996@users.noreply.github.com>
2023-04-13 21:02:44 +0300

    fix the bag in localStorage with ??

--- src/components/Hooks/LocalStorageService.jsx
+++ src/components/Hooks/LocalStorageService.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, arr) {
   const [state, setState] = useState(() =>
-    window.JSON.parse(localStorage.getItem(key) ?? arr)
+    JSON.parse(localStorage.getItem(key)) || arr
   );
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
+    localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
 
   return [state, setState];
